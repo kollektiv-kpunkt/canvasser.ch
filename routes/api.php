3f12c916a15bac820d81f58842f463410f8f6067
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-09-29 21:11:33 +0200

    Change Method of count API point to GET

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post("/buildings/find", [BuildingController::class, "findInTurf"]);
-Route::post("/buildings/count", function () {
+Route::get("/buildings/count", function () {
     return response()->json(["count" => \App\Models\Building::count()]);
 });
 
