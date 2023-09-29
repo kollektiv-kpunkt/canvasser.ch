dd0557804f7925af2a6223d3f6262bde84304517
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-09-29 21:05:54 +0200

    Change Date and time of import

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,8 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('building:import')->quarterlyAt("02:00")->timezone('Europe/Zurich');
-        $schedule->command('zipcode:import')->quarterlyAt("05:00")->timezone('Europe/Zurich');
+        $schedule->command('building:import')->quarterlyOn(1, "02:00")->timezone('Europe/Zurich');
+        $schedule->command('zipcode:import')->quarterlyOn(1, "05:00")->timezone('Europe/Zurich');
     }
 
     /**
